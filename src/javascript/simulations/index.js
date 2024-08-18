94baca19f0f279c61a0683ea9f1fa3e6c5fd5db4
TheTrustyPwo <thetrustypwo@gmail.com>
2024-08-18 15:07:39 +0800

    Fix moving screen on mobile

--- src/javascript/simulations/index.js
+++ src/javascript/simulations/index.js
@@ -9,6 +9,15 @@ class Simulation {
         const clientWidth = document.querySelector(".md-content").clientWidth;
         this.cvs.width = Math.min(1000, clientWidth * 2);
         this.cvs.height = this.cvs.width / 2;
+        this.scale = this.cvs.width / clientWidth;
+    }
+
+    mouseDown() {
+
+    }
+
+    mouseMove(event, x, y) {
+
     }
 }
 
@@ -26,7 +35,8 @@ class Simulation {
             const rect = simulations[i].cvs.getBoundingClientRect();
             if (e.clientX > rect.right || e.clientX < rect.left || e.clientY > rect.bottom || e.clientY < rect.top) continue;
             interacting = i;
-            simulations[interacting].mouseDown(e.clientX - rect.left, e.clientY - rect.top);
+            const scale = simulations[interacting].scale;
+            simulations[interacting].mouseDown(scale * (e.clientX - rect.left), scale * (e.clientY - rect.top));
         }
     });
 });
@@ -45,7 +55,8 @@ window.addEventListener("mousemove", e => {
     if (interacting === -1) return;
     const rect = simulations[interacting].cvs.getBoundingClientRect();
     if (e.clientX > rect.right || e.clientX < rect.left || e.clientY > rect.bottom || e.clientY < rect.top) return;
-    simulations[interacting].mouseMove(e, e.clientX - rect.left, e.clientY - rect.top);
+    const scale = simulations[interacting].scale;
+    simulations[interacting].mouseMove(e, scale * (e.clientX - rect.left), scale * (e.clientY - rect.top));
 });
 
 window.addEventListener("touchmove", e => {
@@ -54,7 +65,8 @@ window.addEventListener("touchmove", e => {
     const rect = simulations[interacting].cvs.getBoundingClientRect();
     if (e.targetTouches[0].clientX > rect.right || e.targetTouches[0].clientX < rect.left
         || e.targetTouches[0].clientY > rect.bottom || e.targetTouches[0].clientY < rect.top) return;
-    simulations[interacting].mouseMove(e, e.targetTouches[0].clientX - rect.left, e.targetTouches[0].clientY - rect.top);
+    const scale = simulations[interacting].scale;
+    simulations[interacting].mouseMove(e, scale * (e.targetTouches[0].clientX - rect.left), scale * (e.targetTouches[0].clientY - rect.top));
 });
 
 const simulations = [];
