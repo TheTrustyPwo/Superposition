f9df6716fff60b7bd77baf7b77564e6bf2b648c4
k3nnaaa <211789b@student.hci.edu.sg>
2025-11-12 10:55:06 +0800

    Update slit.js

--- src/javascript/shared/slit.js
+++ src/javascript/shared/slit.js
@@ -62,24 +62,26 @@ class NSlit {
     }
 
     draw = () => {
-        this.c.beginPath();
-        this.c.strokeStyle = SLITS.COLOR;
-        this.c.lineWidth = SLITS.WIDTH;
-        this.c.moveTo(this.x - this.w / 2, this.y);
-        let dist = this.x - (this.slits * this.width) / 2 - ((this.slits - 1) * this.separation) / 2;
-        this.c.lineTo(dist, this.y);
-        for (let i = 1; i <= this.slits; i++) {
-            dist += this.width;
-            this.c.moveTo(dist, this.y);
-            if (i < this.slits) {
-                dist += this.separation;
-                this.c.lineTo(dist, this.y);
-            }
-        }
-        this.c.lineTo(this.x + this.w / 2, this.y);
-        this.c.closePath();
-        this.c.stroke();
-    }
+      this.c.beginPath();
+      this.c.strokeStyle = SLITS.COLOR;
+      this.c.lineWidth = SLITS.WIDTH;
+
+      // Compute total slit array width in pixels
+      const totalPatternWidth = this.slits * this.width + (this.slits - 1) * this.separation;
+
+      // Start so that the whole pattern is centered at this.x
+      let dist = this.x - totalPatternWidth / 2;
+
+      // Draw each slit
+      for (let i = 0; i < this.slits; i++) {
+        this.c.moveTo(dist, this.y);
+        this.c.lineTo(dist + this.width, this.y);
+        dist += this.width + this.separation;
+      }
+
+      this.c.stroke();
+    };
+
 }
 
 export { Slit, DoubleSlit, NSlit };
