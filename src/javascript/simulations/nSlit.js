a21056b2a5e6f167e0710f9aa02af2c5e3ce2091
k3nnaaa <211789b@student.hci.edu.sg>
2025-11-27 21:42:28 +0800

    Update nSlit.js

--- src/javascript/simulations/nSlit.js
+++ src/javascript/simulations/nSlit.js
@@ -121,7 +121,6 @@ class GratingSimulation {
    ----------------------------- */
 class GratingFFTSimulation {
     constructor(cvs, c, density = 600, wavelength = 500e-9, slitWidth = 2e-6, distanceToScreen = 2.0) {
-        super(cvs, c);
         this.cvs = cvs;
         this.c = c;
 
