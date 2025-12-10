bb47bff0849672a7175ba258c2f54098b88e1469
k3nnaaa <211789b@student.hci.edu.sg>
2025-12-10 19:19:13 +0800

    Update nSlit.js

--- src/javascript/simulations/nSlit.js
+++ src/javascript/simulations/nSlit.js
@@ -1,7 +1,7 @@
 import { Grating } from "../shared/slit.js";
 import { i2h, interpolate, w2h } from "../utils/color.js";
 
-// please this is the main change simulation wise you gotta work 
+// ahhhh this really needs to work please we're behind on deadline 
 
 class GratingFFTSimulation {
   constructor(cvs, ctx, density = 1000, wavelength = 500e-9, slitWidth = 2e-6, distanceToScreen = 2.0) {
@@ -140,7 +140,7 @@ class GratingFFTSimulation {
     const orders = [];
     
     // Fixed number of orders to always display
-    const ordersToShow = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6];
+    const ordersToShow = [-3, -2, -1, 0, 1, 2, 3];
     
     for (const m of ordersToShow) {
       const sinTheta = m * this.wavelength / d;
@@ -303,11 +303,11 @@ class GratingFFTSimulation {
       const nextPeak = peaks[i + 1];
       
       if (i === 0) {
-        // Sharp rise to first peak with slight rounding at top
+        // Sharp rise to first peak with more rounding at top
         const controlX1 = startX + (currentPeak.x - startX) * 0.7;
         const controlY1 = screenY;
-        const controlX2 = currentPeak.x - (currentPeak.x - startX) * 0.08;
-        const controlY2 = screenY - currentPeak.height * 0.85;
+        const controlX2 = currentPeak.x - (currentPeak.x - startX) * 0.15;
+        const controlY2 = screenY - currentPeak.height * 0.75;
         ctx.bezierCurveTo(controlX1, controlY1, controlX2, controlY2, currentPeak.x, screenY - currentPeak.height);
       }
       
@@ -315,9 +315,9 @@ class GratingFFTSimulation {
         const spacing = nextPeak.x - currentPeak.x;
         const midX = (currentPeak.x + nextPeak.x) / 2;
         
-        // Sharp descent from peak with slight rounding at top
-        const descendControlX1 = currentPeak.x + spacing * 0.08;
-        const descendControlY1 = screenY - currentPeak.height * 0.85;
+        // Sharp descent from peak with more rounding at top
+        const descendControlX1 = currentPeak.x + spacing * 0.15;
+        const descendControlY1 = screenY - currentPeak.height * 0.75;
         
         const descendControlX2 = midX - spacing * 0.15;
         const descendControlY2 = screenY + 2;
@@ -325,12 +325,12 @@ class GratingFFTSimulation {
         // Go down to baseline at midpoint
         ctx.bezierCurveTo(descendControlX1, descendControlY1, descendControlX2, descendControlY2, midX, screenY);
         
-        // Sharp ascent to next peak with slight rounding at trough and top
+        // Sharp ascent to next peak with more rounding at both trough and top
         const ascentControlX1 = midX + spacing * 0.15;
         const ascentControlY1 = screenY + 2;
         
-        const ascentControlX2 = nextPeak.x - spacing * 0.08;
-        const ascentControlY2 = screenY - nextPeak.height * 0.85;
+        const ascentControlX2 = nextPeak.x - spacing * 0.15;
+        const ascentControlY2 = screenY - nextPeak.height * 0.75;
         
         // Curve up to next peak
         ctx.bezierCurveTo(ascentControlX1, ascentControlY1, ascentControlX2, ascentControlY2, nextPeak.x, screenY - nextPeak.height);
